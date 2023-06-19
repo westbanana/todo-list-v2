76cadb26f875c754a99639e1af5a3616613a6edf
westbanana <westbanana4@gmail.com>
2023-06-19 23:00:14 +0300

    import Column component, styles, Columns json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import './App.module.scss'
+import Column from "./components/Column/index.jsx";
+import styles from './App.module.scss'
+import Columns from "./data/columns.json"
+const App = () => {
 
-function App() {
   return (
-    <>
-
-    </>
+    <div className={styles.main}>
+        <div className={styles.columnsBlock}>
+          {Columns.map((e, i) => (
+            <Column
+              key={e.name + i}
+              data={Columns[i]}
+            />
+          ))}
+        </div>
+    </div>
   )
 }
 
