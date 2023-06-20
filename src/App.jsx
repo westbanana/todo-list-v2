950a4e881894200bd3333baf5b190c164eddafa1
westbanana <westbanana4@gmail.com>
2023-06-20 22:49:15 +0300

    Changed App.jsx
    Changed:
      key,
      add columns state

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import './App.module.scss'
 import Column from "./components/Column/index.jsx";
 import styles from './App.module.scss'
 import Columns from "./data/columns.json"
+import {useState} from "react";
 const App = () => {
-
+  const [columns, setColumns] = useState(Columns)
+  console.log(columns)
   return (
     <div className={styles.main}>
-        <div className={styles.columnsBlock}>
-          {Columns.map((e, i) => (
+        <div
+          className={styles.columnsBlock}
+        >
+          {columns.map((e, i) => (
             <Column
-              key={e.name + i}
+              key={e.id}
               data={Columns[i]}
+              changeColumns={setColumns}
             />
           ))}
-        </div>
+        </div >
     </div>
   )
 }
