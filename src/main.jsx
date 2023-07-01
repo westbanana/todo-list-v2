2b1f62dbf4c1b336fa116802a1db61e57dcabf40
westbanana <westbanana4@gmail.com>
2023-07-01 15:01:07 +0300

    Update main.jsx
    Add store & provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.module.scss'
+import {Provider} from "react-redux";
+import { store } from './store/index.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
