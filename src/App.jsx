44ea666cdf91f987363fa00122ca4b042a54b2bc
westbanana <westbanana4@gmail.com>
2023-07-01 14:59:23 +0300

    Update App.jsx
    Add useActions;
    Add useSelector:

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,31 @@
 import './App.module.scss'
 import styles from './App.module.scss'
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import ChangeColorMenu from "./components/ChangeColorMenu/index.jsx";
 import Column from "./components/Column/index.jsx";
+import { useSelector} from "react-redux";
+import {useActions} from "./hooks/useActions.js";
 const App = () => {
-  const [columns, setColumns] = useState([
-    {
-      id: 1,
-      title: 'Not started',
-      todos: [
-        {
-          id: 1,
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
-            'molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n' +
-            'numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n' +
-            'optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n' +
-            'obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n' +
-            'nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n' +
-            'tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n' +
-            'quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n' +
-            'sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n' +
-            'recusandae alias error harum maxime adipisci amet laborum. Perspiciatis ',
-        },
-        {
-          id: 2,
-          description: 'Second todo',
-        },
-        {
-          id: 3,
-          description: 'Third todo',
-        },
-        {
-          id: 1,
-          description: 'First todo',
-        },
-        {
-          id: 2,
-          description: 'Second todo',
-        },
-        {
-          id: 3,
-          description: 'Third todo',
-        },
-        {
-          id: 1,
-          description: 'First todo',
-        },
-        {
-          id: 2,
-          description: 'Second todo',
-        },
-        {
-          id: 3,
-          description: 'Third todo',
-        },
-        {
-          id: 1,
-          description: 'First todo',
-        },
-        {
-          id: 2,
-          description: 'Second todo',
-        },
-        {
-          id: 3,
-          description: 'Third todo',
-        },
-      ],
-      color: '#FF1818'
-    },
-    {
-      id: 2,
-      title: 'In progress',
-      todos: [],
-      color: '#1890FF'
-    },
-    {
-      id: 3,
-      title: 'Completed',
-      todos: [],
-      color: '#14DE7D'
-    },
-  ]);
-  const [userBackgroundColor, setUserBackGroundColor] = useState(
-    localStorage.getItem('backgroundColor')
-    ?? 'radial-gradient(circle at 10% 20%, rgb(0, 107, 141) 0%, rgb(0, 69, 91) 90%)'
-  );
-  
+  const { columns } = useSelector(prev => prev.columns)
+  const { backGroundColor } = useSelector(prev => prev.background)
+  useEffect(() => {
+    const columnsJSON = JSON.stringify(columns)
+    localStorage.setItem('columns', columnsJSON);
+  }, [columns])
+  const {
+    changeBackgroundColor,
+  } = useActions()
   return (
     <div
       style={{
-        background: userBackgroundColor,
+        background: backGroundColor,
       }}
       id='mainBackgroundId'
       className={styles.main}
     >
       <ChangeColorMenu
-        initialValue={userBackgroundColor}
-        callback={setUserBackGroundColor}
+        initialValue={backGroundColor}
+        callback={changeBackgroundColor}
       />
       <div
         id='blurBlock'
