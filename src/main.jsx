f0d3c69c548de82b5c6edd0e88655ff9e7a7ef8b
westbanana <93942346+westbanana@users.noreply.github.com>
2023-07-04 17:47:22 +0300

    React buddy wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.module.scss'
 import {Provider} from "react-redux";
-import { store } from './store/index.js';
+import {store} from './store/index.js';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <DevSupport ComponentPreviews={ComponentPreviews}
+                useInitialHook={useInitial}
+    >
+      <App/>
+    </DevSupport>
   </Provider>
 )
