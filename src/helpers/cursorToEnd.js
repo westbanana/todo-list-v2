f566262705224c8812e71485d4a2242f7cfa2568
westbanana <westbanana4@gmail.com>
2023-07-15 03:39:38 +0300

    Create cursorToEnd

---
+++ src/helpers/cursorToEnd.js
@@ -0,0 +1,8 @@
+export const cursorToEnd = (element) => {
+  const range = document.createRange();
+  const selection = window.getSelection();
+  range.selectNodeContents(element);
+  range.collapse(false);
+  selection.removeAllRanges();
+  selection.addRange(range);
+};
